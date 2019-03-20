2eb8505632f3e2fe5892a8350373dc8b54a9a1a0
Lucas Akira Uehara <Akira@akira.com>
2019-03-19 21:42:22 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgxViaCepService } from '@ksakira10/ngx-via-cep';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NgxViaCepService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'base-project';
 
-  constructor(private ngxViaCepService: NgxViaCepService) {
+  constructor(private ngxViaCepService: NgxViaCepService) {}
 
+  ngOnInit() {
+    this.ngxViaCepService
+      .fetchByCEP('03911000')
+      .subscribe(data => console.log(data));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxViaCepModule } from '@ksakira10/ngx-via-cep';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxViaCepModule
   ],
   providers: [],
   bootstrap: [AppComponent]
