e541932f81ad54244061d0ed8d2d61aa0f2e50bd
Lucas Akira Uehara <akirauehara@hotmail.com>
2019-03-23 14:47:06 -0300

    refact build prod

--- src/app/pages/ngx-via-cep/ngx-via-cep.module.ts
+++ src/app/pages/ngx-via-cep/ngx-via-cep.module.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  NgxViaCepModule as ViaCepModule,
-  NgxViaCepConfiguration
-} from '@ksakira10/ngx-via-cep';
+import { NgxViaCepModule as ViaCepModule } from '@ksakira10/ngx-via-cep';
 import { NgxViaCepRoutingModule } from './ngx-via-cep-routing.module';
 import { NgxViaCepComponent } from './ngx-via-cep.component';
 
@@ -15,9 +12,7 @@ import { NgxViaCepComponent } from './ngx-via-cep.component';
     CommonModule,
     NgxViaCepRoutingModule,
     HttpClientModule,
-    ViaCepModule.forRoot(
-      () => new NgxViaCepConfiguration({ basePath: 'https://viacep.com.br/ws' })
-    ),
+    ViaCepModule.forRoot(null),
     ReactiveFormsModule
   ]
 })
