f7e262e0b38d668c26aee9739ac47cbb6c3e1521
Lucas Akira Uehara <akirauehara@hotmail.com>
2019-03-23 19:22:16 -0300

    add autocomplete reactive

--- src/app/pages/ngx-via-cep/ngx-via-cep.component.ts
+++ src/app/pages/ngx-via-cep/ngx-via-cep.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgxViaCepService } from '@ksakira10/ngx-via-cep';
+import { Subject } from 'rxjs';
+import {
+  takeUntil,
+  tap,
+  map,
+  switchMap,
+  groupBy,
+  reduce,
+  filter
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-ngx-via-cep',
@@ -8,10 +18,13 @@ import { NgxViaCepService } from '@ksakira10/ngx-via-cep';
   styleUrls: ['./ngx-via-cep.component.scss'],
   providers: [NgxViaCepService]
 })
-export class NgxViaCepComponent implements OnInit {
+export class NgxViaCepComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<boolean>();
   viaCepForm: FormGroup;
   cep: FormControl;
+  endereco: FormControl;
   result: string;
+  logradouros: string[] = [];
   constructor(private viaCepService: NgxViaCepService) {
     this.cep = new FormControl(
       { value: '', disabled: false },
@@ -24,12 +37,24 @@ export class NgxViaCepComponent implements OnInit {
         updateOn: 'submit'
       }
     );
+    this.endereco = new FormControl('', {
+      validators: [Validators.minLength(5)],
+      updateOn: 'change'
+    });
     this.viaCepForm = new FormGroup({
-      cep: this.cep
+      cep: this.cep,
+      endereco: this.endereco
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerValueChanges();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 
   fetchByCEP(form: FormGroup) {
     if (form.invalid) {
@@ -37,8 +62,45 @@ export class NgxViaCepComponent implements OnInit {
     }
     const { fetchByCEP } = this.viaCepService;
     const { cep } = form.value;
-    fetchByCEP(cep).subscribe(
-      data => (this.result = JSON.stringify(data, null, 2))
-    );
+    fetchByCEP(cep)
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(data => (this.result = JSON.stringify(data, null, 2)))
+      )
+      .subscribe();
+  }
+
+  private registerValueChanges() {
+    this.endereco.valueChanges
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(address => {
+          const { fetchByAddressSaoPaulo } = this.viaCepService;
+          if (address.length >= 5) {
+            fetchByAddressSaoPaulo(address)
+              .pipe(
+                takeUntil(this.destroy$),
+                tap(data => (this.result = JSON.stringify(data, null, 2))),
+                map(data => {
+                  let logradouro;
+                  const logradouros = [];
+                  data.map(data => {
+                    if (!logradouro) {
+                      logradouro = data.logradouro;
+                      logradouros.push(data.logradouro);
+                    } else if (logradouro !== data.logradouro) {
+                      logradouro = data.logradouro;
+                      logradouros.push(data.logradouro);
+                    }
+                  });
+                  return logradouros;
+                }),
+                tap(data => (this.logradouros = data)),
+              )
+              .subscribe();
+          }
+        })
+      )
+      .subscribe();
   }
 }
