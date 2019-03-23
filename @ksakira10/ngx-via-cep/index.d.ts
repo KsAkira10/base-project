2893e9ca852a9fdf541c5437b32c38cfd8993b0d
Lucas Akira Uehara <akirauehara@hotmail.com>
2019-03-23 13:19:41 -0300

    Auto-generated commit

--- @ksakira10/ngx-via-cep/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/ngx-via-cep.variables';
-export * from './lib/ngx-via-cep.configuration';
-export * from './lib/ngx-via-cep.service';
-export * from './lib/ngx-via-cep.module';
--- @ksakira10/ngx-via-cep/lib/ngx-via-cep.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { ModuleWithProviders } from '@angular/core';
-import { NgxViaCepConfiguration } from './ngx-via-cep.configuration';
-export declare class NgxViaCepModule {
-    static forRoot(configurationFactory: () => NgxViaCepConfiguration): ModuleWithProviders;
-    constructor(parentModule: NgxViaCepModule, http: HttpClient);
-}
