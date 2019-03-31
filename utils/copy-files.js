4afa6f5544ffd5c240b58a61c2ae6339b8e551cc
Lucas Akira Uehara <akirauehara@hotmail.com>
2019-03-31 17:01:01 -0300

    add callback to path

--- utils/copy-files.js
+++ utils/copy-files.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const currentPath = process.cwd();
-// destination.txt will be created or overwritten by default.
 fs.copyFile(`${currentPath}/dist/base-project/index.html`, `${currentPath}/dist/base-project/404.html`, (err) => {
   if (err) throw err;
   console.log('success in copy');
